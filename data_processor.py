d3c3d107e00bf4df3eb9ce5c1bc862409ade6448
Karl Thilking <thilking.k@northeastern.edu>
2025-11-25 01:42:51 -0500

    added function for initializing all piano and violin note sequences

--- data_processor.py
+++ data_processor.py
@@ -9,61 +9,42 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 
 class DataProcessor:
-  def __init__(self, midi_path='piano-violin-data'):
-    self.midi_files = list(Path(midi_path).glob('*.mid'))[:5000]
-    self.midi_objects = None
+  def __init__(self, midi_path='piano-violin-data', num_songs=2500):
+    self.midi_files = list(Path(midi_path).glob('*.mid'))[:num_songs]
+    self.midi_objects: List[pm.PrettyMIDI]
+    self.all_piano_notes: List[int]
+    self.all_violin_notes: List[int]
 
   @staticmethod
   def hash_note(note: List[Any]) -> int:
-    '''
-    Input: list of pitch (int), velocity (int), duration (float), is start (0 or 1), is piano (0 or 1)
-
-    Pitch: integer in (0, 127) -> 7 bits 
-    Velocity: integer in (0, 127) -> 7 bits 
-    Duration: float in (0, 2.xx) -> multiply by 100 for integer that include 2 decimal points -> integer in (0, ~256) -> 8 bits
-    Start: 1 if start note else 0 -> 1 bit 
-    Instrument: 1 if piano (harmony) else if violin 0 (melody) -> 1 bit
-
-    Returns: 30 bit integer representing pitch, velocity, duration, start, and instrument
-    '''
     duration = min(255, int(note[2] * 100))
     return (note[0] & 0x7F) | (note[1] & 0x7F) << 7 | (duration & 0xFF) << 14 | (note[3] & 1) << 22 | (note[4] & 1) << 23
 
-  @staticmethod
-  def get_piano_notes(midi: pm.PrettyMIDI) -> List[Any]:
+  @staticmethod 
+  def get_notes(midi: pm.PrettyMIDI) -> Tuple[List[int], List[int]]:
     '''
-    Return list of notes of the form [p, v, d, is_s, is_p] where:
-    p: pitch -> integer(0, 127)
-    v: velocity -> integer(0, 127)
-    d: duration -> float
-    is_s: is the note a start note -> 1 if so 0 otherwise
-    is_p: is the instrument a piano -> 1 if so 0 otherwise 
+    Input: individual midi object
+    Returns: list of piano notes, list of violin notes
+
+    For each note in the midi object, notes of the form [pitch, velocity, duration, is start note, is piano]
+    are hashed into a single integer and added to the respective sequence (piano notes or violin notes).
     '''
-    piano_notes = []
+    piano_notes, violin_notes = [], []
     for instrument in midi.instruments:
       name = pm.program_to_instrument_name(instrument.program)
-      if 'piano' not in name:
-        continue 
-      for i, n in enumerate(instrument.notes):
-        if i == 0:
-          piano_notes.append([n.pitch, n.velocity, n.get_duration(), 1, 1])
-        else:
-          piano_notes.append([n.pitch, n.velocity, n.get_duration(), 0, 1])
-    return piano_notes
-  
-  @staticmethod
-  def get_violin_notes(midi: pm.PrettyMIDI) -> List[Any]:
-    violin_notes = []
-    for instrument in midi.instruments: 
-      name = pm.program_to_instrument_name(instrument.program)
-      if name.lower() != 'violin':
-        continue 
-      for i, n in enumerate(instrument.notes):
-        if i == 0:
-          violin_notes.append([n.pitch, n.velocity, n.get_duration(), 1, 0])
-        else:
-          violin_notes.append([n.pitch, n.velocity, n.get_duration(), 0, 0])
-    return violin_notes
+      if 'piano' in name.lower():
+        for i, n in enumerate(instrument.notes):
+          if i == 0:
+            piano_notes.append([n.pitch, n.velocity, n.get_duration(), True, True])
+          else:
+            piano_notes.append([n.pitch, n.velocity, n.get_duration(), False, True])
+      elif name.lower() == 'violin':
+        for i, n in enumerate(instrument.notes):
+          if i == 0:
+            violin_notes.append([n.pitch, n.velocity, n.get_duration(), True, False])
+          else:
+            violin_notes.append([n.pitch, n.velocity, n.get_duration(), False, False])
+    return piano_notes, violin_notes
 
   @staticmethod
   def process_midi_file(midi_file: Path) -> Union[pm.PrettyMIDI, None]:
@@ -80,5 +61,22 @@ class DataProcessor:
       midi_objects = [f for f in futures if f]
     self.midi_objects = midi_objects
 
+  def load_piano_sequence(self):
+    if not self.midi_objects:
+      print('Piano sequences cannot be loaded yet')
+      return
+    raw_piano_notes = []
+    for midi in tqdm(self.midi_objects):
+      raw_piano_notes.extend(self.get_piano_notes(midi))
+    all_piano_notes = []
+    for note in raw_piano_notes:
+      all_piano_notes.append(self.hash_note(note))
+    self.all_piano_notes = all_piano_notes
+  
+  def load_violin_sequence(self):
+    if not self.midi_objects:
+      print('Violin sequences cannot be loaded yet')
+      return
+    
 
   
