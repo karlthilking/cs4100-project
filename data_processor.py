46947d7969547037b2d81aef43ecf3dbe360ead5
Karl Thilking <thilking.k@northeastern.edu>
2025-11-25 00:23:59 -0500

    added hash function, functions for extracting piano or violin notes from a midi file, and functions for processing midi files into pretty_midi midi objects

--- data_processor.py
+++ data_processor.py
@@ -11,29 +11,59 @@ import os
 class DataProcessor:
   def __init__(self, midi_path='piano-violin-data'):
     self.midi_files = list(Path(midi_path).glob('*.mid'))[:5000]
-    self.midi_objects = self.get_midi_objects() 
+    self.midi_objects = None
 
-  def get_piano_notes(self, midi_file: Path) -> List[Any]:
-    midi = pm.PrettyMIDI(midi_file)
+  @staticmethod
+  def hash_note(note: List[Any]) -> int:
+    '''
+    Input: list of pitch (int), velocity (int), duration (float), is start (0 or 1), is piano (0 or 1)
+
+    Pitch: integer in (0, 127) -> 7 bits 
+    Velocity: integer in (0, 127) -> 7 bits 
+    Duration: float in (0, 2.xx) -> multiply by 100 for integer that include 2 decimal points -> integer in (0, ~256) -> 8 bits
+    Start: 1 if start note else 0 -> 1 bit 
+    Instrument: 1 if piano (harmony) else if violin 0 (melody) -> 1 bit
+
+    Returns: 30 bit integer representing pitch, velocity, duration, start, and instrument
+    '''
+    duration = min(255, int(note[2] * 100))
+    return (note[0] & 0x7F) | (note[1] & 0x7F) << 7 | (duration & 0xFF) << 14 | (note[3] & 1) << 22 | (note[4] & 1) << 23
+
+  @staticmethod
+  def get_piano_notes(midi: pm.PrettyMIDI) -> List[Any]:
+    '''
+    Return list of notes of the form [p, v, d, is_s, is_p] where:
+    p: pitch -> integer(0, 127)
+    v: velocity -> integer(0, 127)
+    d: duration -> float
+    is_s: is the note a start note -> 1 if so 0 otherwise
+    is_p: is the instrument a piano -> 1 if so 0 otherwise 
+    '''
     piano_notes = []
     for instrument in midi.instruments:
       name = pm.program_to_instrument_name(instrument.program)
-      if 'piano' not in name.lower():
-        continue
-      for note in instrument.notes:
-        piano_notes.append([note.start, note.end - note.start, note.pitch, note.velocity])
-    return sorted(piano_notes, key=lambda x: x[0])
+      if 'piano' not in name:
+        continue 
+      for i, n in enumerate(instrument.notes):
+        if i == 0:
+          piano_notes.append([n.pitch, n.velocity, n.get_duration(), 1, 1])
+        else:
+          piano_notes.append([n.pitch, n.velocity, n.get_duration(), 0, 1])
+    return piano_notes
   
-  def get_violin_notes(self, midi_file: Path) -> List[Any]:
-    midi = pm.PrettyMIDI(midi_file)
+  @staticmethod
+  def get_violin_notes(midi: pm.PrettyMIDI) -> List[Any]:
     violin_notes = []
-    for instrument in midi.instruments:
+    for instrument in midi.instruments: 
       name = pm.program_to_instrument_name(instrument.program)
       if name.lower() != 'violin':
         continue 
-      for note in instrument.notes:
-        violin_notes.append([note.start, note.end - note.start, note.pitch, note.velocity])
-    return sorted(violin_notes, key=lambda x: x[0])
+      for i, n in enumerate(instrument.notes):
+        if i == 0:
+          violin_notes.append([n.pitch, n.velocity, n.get_duration(), 1, 0])
+        else:
+          violin_notes.append([n.pitch, n.velocity, n.get_duration(), 0, 0])
+    return violin_notes
 
   @staticmethod
   def process_midi_file(midi_file: Path) -> Union[pm.PrettyMIDI, None]:
@@ -43,15 +73,12 @@ class DataProcessor:
     except:
       return None 
     
-  def get_midi_objects(self, num_threads=16) -> List[pm.PrettyMIDI]:
+  def load_midi_objects(self, num_threads=16):
     midi_objects = []
     with ThreadPoolExecutor(num_threads) as executor:
       futures = executor.map(self.process_midi_file, self.midi_files)
       midi_objects = [f for f in futures if f]
-    return midi_objects
+    self.midi_objects = midi_objects
 
-if __name__ == "__main__":
-  dp = DataProcessor()
-  print(len(dp.midi_files))
-  print(len(dp.midi_objects))
+
   
