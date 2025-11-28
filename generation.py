da956a1ecd462265ed87c60a09b27e17ab79a903
ErginaAgastra <erginaagastra@gmail.com>
2025-11-28 07:04:53 -0500

    Updated decode_note to unpack hashed notes based on new data processing and updated main based on new train/test split

--- generation.py
+++ generation.py
@@ -7,22 +7,38 @@ import random
 
 # decode from hash to MIDI information
 def decode_note(note_hash: int) -> Tuple[int, int, float]:
-    pitch = note_hash & 0x7F
-    velocity = (note_hash >> 7) & 0x7F
-    dur_bin = (note_hash >> 14) & 0x7 
+    dur_bin      =  note_hash        & 0x3          
+    octave_bin   = (note_hash >> 2)  & 0x3        
+    velo_bin     = (note_hash >> 4)  & 0x7        
+    pitch_class  = (note_hash >> 7)  & 0xF 
 
-    piano_bin_durations = [
-        0.08,   
-        0.125,  
-        0.18,   
-        0.25,  
-        0.35,   
-        0.5,    
-        1.2,    
-        2.0,    
+    # TODO: could be tweaked
+    OCTAVE_BIN_TO_OCT = [2, 3, 4, 5]  # you can tweak these if needed
+
+    # use midpoints as representative values
+    VEL_BIN_TO_VEL = [
+        8,  
+        24, 
+        40,  
+        56,  
+        72,  
+        88,
+        104,
+        120,
+    ]
+    
+    DUR_BIN_TO_SEC_PIANO = [
+        0.10,  
+        0.22,  
+        0.45,  
+        0.80,  
     ]
 
-    duration = piano_bin_durations[dur_bin]
+    octave = OCTAVE_BIN_TO_OCT[octave_bin]
+    pitch = (octave + 1) * 12 + pitch_class
+    velocity = VEL_BIN_TO_VEL[velo_bin]
+    duration = DUR_BIN_TO_SEC_PIANO[dur_bin]
+
     return pitch, velocity, duration
 
 # take in a sequence and turn it into a midi file
@@ -75,25 +91,25 @@ def generate_harmony(
     return best_path, best_log_prob
 
 def main():
-    dp = DataProcessor()
-    dp.init_note_sequences()
+    # Train HMM on train set
+    dp_train = DataProcessor(train=True)
+    dp_train.init_note_sequences()
+    dp_train.init_hmm()
+    T_prob, O_prob, pi_prob, states = dp_train.get_hmm_params()
 
-    # pick a random violin melody as a test
-    non_empty_indices = [i for i, seq in enumerate(dp.violin_sequences) if seq]
+    # Load testing melodies
+    dp_test = DataProcessor(train=False)
+    dp_test.init_note_sequences()
+
+    # violin_sequences are
+    violin_seqs = dp_test._DataProcessor__violin_sequences 
+
+    # pick a random test melody
+    non_empty_indices = [i for i, seq in enumerate(violin_seqs) if len(seq) > 0]
     test_idx = random.choice(non_empty_indices)
 
-    raw_melody = dp.violin_sequences[test_idx]
+    raw_melody = violin_seqs[test_idx]
     observations = [DataProcessor.hash_note(n, is_piano=False) for n in raw_melody]
-
-    # build training sets on all melodies, except the testing one
-    train_piano = [seq for i, seq in enumerate(dp.piano_sequences) if i != test_idx]
-    train_violin = [seq for i, seq in enumerate(dp.violin_sequences) if i != test_idx]
-
-    # HMM trained on N-1 songs
-    dp._DataProcessor__piano_sequences = train_piano
-    dp._DataProcessor__violin_sequences = train_violin
-    dp.init_hmm()
-    T_prob, O_prob, pi_prob, states = dp.get_hmm_params()
 
     midi_path = "harmony_final.mid"
     sheet_path = "harmony_final.musicxml"
@@ -107,6 +123,9 @@ def main():
         midi_out=midi_path,
         sheet_out=sheet_path,
     )
-
+    
+    print("Test melody idx:", test_idx)
+    print("Best log-prob:", best_log_prob)
+    
 if __name__ == "__main__":
     main()
