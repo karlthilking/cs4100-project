1607885352012a03f4da0c4fe8a09ea2b8f27685
Alyssa Chang <alyssachang109@gmail.com>
2025-12-08 13:06:24 -0500

    updated hmm_2 to have rewards

--- hmm_2.py
+++ hmm_2.py
@@ -17,6 +17,57 @@ class HMM:
     self.__log_O = np.log(self.__O)
     self.__log_pi = np.log(self.__pi)
 
+  def interval_consonance_reward(self, melody_note, harmony_note):
+    melody_pitch = (melody_note  >> 6) & 0xF
+    harmony_pitch = (harmony_note >> 6) & 0xF
+    interval = abs(melody_pitch - harmony_pitch) % 12
+
+    perfect_intervals = {0, 5, 7, 12}
+    imperfect_intervals = {3, 4, 8, 9}
+    dissonant_intervals = {1, 2, 6, 10, 11}
+          
+    if interval in perfect_intervals:
+      return 0.7
+    elif interval in imperfect_intervals:
+      return 0.5
+    else:
+      return 0.3
+
+  def harmony_octave_penalty(self, prev_s, s):
+    '''
+    Implements small/medium punishment for the harmony jumping between octaves 
+    ie. encouraging the harmony to be much smoother
+    '''
+    # get the pitches including the octave
+    prev_pitch = ((prev_s >> 6) & 0xF) + 12 * ((prev_s >> 2) & 0x3)
+    curr_pitch = ((s      >> 6) & 0xF) + 12 * ((s      >> 2) & 0x3)
+    diff = abs(prev_pitch - curr_pitch)
+
+    # no penalty for within an octave
+    if diff == 0:
+      return 0.0
+    # small penalty if within 2 octaves
+    elif diff == 1:
+      return -0.5
+    # larger penalty greater than 2 octave differences
+    else:
+      return -1.0
+        
+
+  def duration_reward(self, melody_note, harmony_note):
+    '''
+    Rewards the harmony for matching the melody duration according to their bins
+    NOTE: the binned durations for melodies and harmonies differ
+    '''
+    melody_duration = (melody_note  >> 0) & 0x3
+    harmony_duration = (harmony_note >> 0) & 0x3
+        
+    # rewards for harmony note matching the duration of the melody (in bin)
+    if melody_duration == harmony_duration:
+      return 1
+    else: 
+      return 0    
+
   def viterbi(self, observations):
     # initialize variables
     T = len(observations); N = len(self.__states)
@@ -31,7 +82,43 @@ class HMM:
     for t in tqdm(range(1, T), total=T):
       temp = V[t - 1, :].reshape(-1, 1) + self.__log_T
       back[t, :] = np.argmax(temp, axis=0)
-      V[t, :] = temp[back[t, :], np.arange(N)] + self.__log_O[:, obs_ix[t]]
+      base_score = temp[back[t, :], np.arange(N)]
+
+      # get the current melody note
+      melody_note = observations[t]
+
+      # duration reward
+      dur_reward = np.array([
+        self.duration_reward(melody_note, self.__states[s])
+        for s in range(N)
+      ])
+
+      # interval reward multiplier
+      interval_reward_multiplier = np.array([
+        self.interval_consonance_reward(melody_note, self.__states[s])
+        for s in range(N)
+      ])
+
+      # jump penalty 
+      jump_penalty = np.array([
+        self.harmony_octave_penalty(
+          self.__states[back[t, s]],     
+          self.__states[s]       
+        )
+        for s in range(N)
+      ])
+
+      # combine rewards into score
+      score = (
+        base_score
+        + self.__log_O[:, obs_ix[t]]
+        + dur_reward
+        + jump_penalty
+      )
+
+      score *= interval_reward_multiplier
+
+      V[t, :] = score
     
     # termination
     last_ix = np.argmax(V[-1, :])
